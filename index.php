ee11345bbc7299b16a5fac3e64eaf494f2b09f0e
Camilo Diaz <info@enteraweb.co>
2017-03-09 00:18:24 -0500

    correccion ws
    texto de mas

--- index.php
+++ index.php
@@ -2473,7 +2473,7 @@
                             </div>
                             <div class="caption before-media">
                                 <h3 class="caption-title">Speaker name here</h3>
-                                <p class="caption-category"> CONFECOLws </p>
+                                <p class="caption-category"> CONFECOL </p>
                             </div>
                             <div class="caption">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed vel velit</p>
