c60b6159de443856208a8dd453adb61474be714f
Camilo Diaz <info@enteraweb.co>
2017-03-15 17:40:00 -0500

    Hotel Casa y el mirador

---
+++ hotel-mirador-del-recuerdo.php
@@ -0,0 +1,276 @@
+<?php require_once('menu.php'); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="es"><!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Hotel Mirador del Recuerdo </title>
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <!-- CSS Global -->
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/plugins/fontawesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/plugins/bootstrap-select/bootstrap-select.min.css" rel="stylesheet">
+    <link href="assets/plugins/owlcarousel2/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/plugins/owlcarousel2/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link href="assets/plugins/prettyphoto/css/prettyPhoto.css" rel="stylesheet">
+    <link href="assets/plugins/animate/animate.min.css" rel="stylesheet">
+
+    <link href="assets/css/theme.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="assets/plugins/iesupport/html5shiv.js"></script>
+    <script src="assets/plugins/iesupport/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="home" class="wide body-light sub-page">
+
+<!-- Preloader -->
+<div id="preloader">
+    <div id="status">
+        <div class="spinner"></div>
+    </div>
+</div>
+
+<!-- Wrap all content -->
+<div class="wrapper">
+
+    <!-- HEADER -->
+    <header class="header fixed">
+        <div class="container">
+            <div class="header-wrapper clearfix">
+
+            <!-- Logo -->
+            <div class="logo">
+                <a href="#home" class="scroll-to">
+                    <span class="fa-stack">
+                        <i class="fa logo-hex fa-stack-2x"></i>
+                        <i class="fa logo-fa fa-map-marker fa-stack-1x"></i>
+                    </span>
+                    4CES
+                </a>
+            </div>
+            <!-- /Logo -->
+
+            <!-- Navigation -->
+            <div id="mobile-menu"></div>
+            <?php echo @menu($menu); ?>
+            <!-- /Navigation -->
+
+            </div>
+        </div>
+    </header>
+    <!-- /HEADER -->
+
+    <!-- Content area -->
+    <div class="content-area">
+
+        <!-- PAGE BLOG -->
+        <section class="page-section with-sidebar sidebar-right">
+        <div class="container">
+        <div class="row">
+
+        <!-- Content -->
+        <section id="content" class="content col-sm-8 col-md-9">
+
+            <article class="post-wrap" data-animation="fadeInUp" data-animation-delay="100">
+                <div class="post-media">
+                    <img src="assets/img/hoteles/mirador.jpg" alt="Hotel Boutique City Center"/>
+                </div>
+                <div class="post-header">
+                    <h2 class="post-title"><a href="#"> Hotel Mirador del Recuerdo </a></h2>
+
+                </div>
+                <div class="post-body">
+                    <div class="post-excerpt">
+                    <p><strong>Direccion:</strong> Cra. 39ª 25-50 / Bogotá - Colombia.<br />
+                    <strong>Telefono:</strong> (571) - 743 34 60.<br />
+                    <strong>Pagina Web: </strong><a href="https://www.miradordelrecuerdo.com" target="_blank"> https://www.miradordelrecuerdo.com </a></p>
+<hr>
+                    <h2 class="post-title"><a href="#"> Tarifas </a></h2>
+                        <table width="80%" border="0" align="center" cellpadding="3" cellspacing="3">
+                          <tr>
+                            <th scope="col"><div align="center"> Acomodación Sencilla </div></th>
+                            <th scope="col"><div align="center"> Acomodación Doble</div></th>
+                            <th scope="col"><div align="center"> Acomodación Triple</div></th>
+                            <th scope="col"><div align="center"> Acomodación cuadruple</div></th>
+                          </tr>
+                          <tr>
+                            <td>
+                            <div align="center"> $150.000 + IVA + SH $5.000 </div>
+                            </td>
+                            <td><div align="center">
+                              <div> $170.000 + IVA + SH $10.000 </div>
+                            </div></td>
+                            <td><div align="center">
+                              <div> $210.000 + IVA + SH $15.000 </div>
+                            </div></td>
+                            <td><div align="center">
+                              <div> $280.000+ IVA + SH $20.000 </div>
+                            </div></td>
+                          </tr>
+                          <tr>
+                            <td><div align="center">
+                              <div> $ 183.500 </div>
+                            </div></td>
+                            <td><div align="center">
+                              <div> $ 212.300 </div>
+                            </div></td>
+                            <td><div align="center">
+                              <div> $ 264.900 </div>
+                            </div></td>
+                            <td><div align="center"> $ 353.200 </div></td>
+                          </tr>
+                        </table>
+                    <hr>
+                    <h2 class="post-title"><a href="#"> Valores agregados </a></h2>
+
+                            <ul style="list-style-type:circle">
+                              <li> Traslado Aeropuerto Hotel Aeropuerto (importante informar los itinerarios para el servicio de protocolo). </li>
+                              <li> Desayuno Tipo Buffet. </li>
+                              <li> Asistencia Médica 24 horas. </li>
+                              <li> Internet Wi-Fi. </li>
+                              <li> Llamadas locales sin costo los primeros 10 minuto. </li>
+                              <li> Cajillas de seguridad. </li>
+                              <li> Salón de cortesía con 20 pax alojados. </li>
+                            </ul>
+
+                    </div>
+                </div>
+            </article>
+
+            <!-- About the author -->
+
+            <!-- /About the author -->
+
+            <!-- Comments -->
+
+            <!-- /Comments -->
+
+            <!-- Leave a Comment -->
+
+            <!-- /Leave a Comment -->
+
+        </section>
+        <!-- Content -->
+
+        <hr class="page-divider transparent visible-xs"/>
+
+        <!-- Sidebar -->
+        <aside id="sidebar" class="sidebar col-sm-4 col-md-3">
+
+            <div class="widget categories">
+                <h4 class="widget-title">Categories</h4>
+                <ul>
+                    <li><a href="#">Design <small>14</small></a></li>
+                    <li><a href="#">Art Direction <small>21</small></a></li>
+                    <li class="active"><a href="#">HTML and CSS <small>12</small></a></li>
+                    <li><a href="#">Themes <small>19</small></a></li>
+                    <li><a href="#">Wordpress <small>36</small></a></li>
+                </ul>
+            </div>
+
+            <div class="widget flickr-feed">
+                <h4 class="widget-title">Flickr Feed</h4>
+                <ul>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                    <li><a href="#"><img src="assets/img/preview/flickr-feed.jpg" alt="//"/></a></li>
+                </ul>
+            </div>
+
+            <div class="widget tag-cloud">
+                <h4 class="widget-title">Tags</h4>
+                <ul>
+                    <li><a href="#">Creative</a></li>
+                    <li><a href="#">Design</a></li>
+                    <li><a href="#">Art</a></li>
+                    <li><a href="#">Autpor Name</a></li>
+                    <li><a href="#">Coorperate</a></li>
+                    <li><a href="#">Wordpress</a></li>
+                    <li><a href="#">3D Animations</a></li>
+                </ul>
+            </div>
+
+            <div class="widget">
+                <h4 class="widget-title">Join The Newsletter</h4>
+                <form method="post" action="#">
+                    <div class="form-group">
+                        <input type="text" placeholder="Type and hit enter... " class="form-control" name="search" title="search">
+                        <input type="submit" hidden="hidden"/>
+                    </div>
+                </form>
+            </div>
+
+        </aside>
+        <!-- Sidebar -->
+
+        </div>
+        </div>
+        </section>
+        <!-- /PAGE BLOG -->
+
+    </div>
+    <!-- /Content area -->
+
+    <!-- FOOTER -->
+    <?php echo @footer($footer); ?>
+    <!-- /FOOTER -->
+
+    <div class="to-top"><i class="fa fa-angle-up"></i></div>
+
+</div>
+<!-- /Wrap all content -->
+
+<!-- JS Global -->
+
+<!--[if lt IE 9]><script src="assets/plugins/jquery/jquery-1.11.1.min.js"></script><![endif]-->
+<!--[if gte IE 9]><!--><script src="assets/plugins/jquery/jquery-2.1.1.min.js"></script><!--<![endif]-->
+<script src="assets/plugins/modernizr.custom.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/plugins/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/plugins/superfish/js/superfish.js"></script>
+<script src="assets/plugins/prettyphoto/js/jquery.prettyPhoto.js"></script>
+<script src="assets/plugins/placeholdem.min.js"></script>
+<script src="assets/plugins/jquery.smoothscroll.min.js"></script>
+<script src="assets/plugins/jquery.easing.min.js"></script>
+
+<!-- JS Page Level -->
+
+<script src="assets/js/theme-ajax-mail.js"></script>
+<script src="assets/js/theme.js"></script>
+<script src="assets/js/custom.js"></script>
+
+<!--[if (gte IE 9)|!(IE)]><!-->
+<!--script src="assets/plugins/jquery.cookie.js"></script>
+<script src="assets/js/theme-config.js"></script-->
+<!--<![endif]-->
+
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        theme.init();
+    });
+
+    //jQuery(window).load(function () { jQuery('body').scrollspy({offset: 100, target: '.navigation'}); });
+    //jQuery(window).load(function () { jQuery('body').scrollspy('refresh'); });
+    //jQuery(window).resize(function () { jQuery('body').scrollspy('refresh'); });
+
+</script>
+
+</body>
+</html>
